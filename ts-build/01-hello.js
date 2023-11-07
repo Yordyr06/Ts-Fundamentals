bb4de65cdd6a95908d4c7c115c81a7ff822be9b0
Yordy Almonte <yoalmonte.dev@outlook.com>
2023-11-07 08:42:56 +0000

    Js Deleted

--- ts-build/01-hello.js
+++
@@ -1,2 +0,0 @@
-var nombre = "Yordy Almonte";
-console.log(nombre);
--- ts-build/03-typing.js
+++
@@ -1,8 +0,0 @@
-(function () {
-    var myProductName = "apple";
-    var myProductNumber = 123;
-    myProductName = "change";
-    myProductName.toUpperCase();
-    myProductNumber.toFixed();
-    var example = 0;
-})();
