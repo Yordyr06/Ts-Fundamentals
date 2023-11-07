219586affc01a396896cb4e521e3fae18396897c
Yordy Almonte <yoalmonte.dev@outlook.com>
2023-11-07 06:46:47 +0000

    Type of types

---
+++ ts-build/03-typing.js
@@ -0,0 +1,8 @@
+(function () {
+    var myProductName = "apple";
+    var myProductNumber = 123;
+    myProductName = "change";
+    myProductName.toUpperCase();
+    myProductNumber.toFixed();
+    var example = 0;
+})();
