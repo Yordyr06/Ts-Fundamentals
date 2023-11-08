a6fe515fdb553db4e0dd312bced8a15fbf63740c
Yordy Almonte <yoalmonte.dev@outlook.com>
2023-11-08 08:21:28 +0000

    Union Types

---
+++ src/09-union-types.ts
@@ -0,0 +1,16 @@
+(() => {
+  let userId: string | number;
+  userId = 2313213;
+  userId = "asdasd";
+
+  function greeting(myCharacter: string | number) {
+    if (typeof myCharacter === "string") {
+      console.log(`My Character is a string: ${myCharacter.toLowerCase()}`);
+    } else {
+      console.log(`My Character is a number: ${myCharacter.toFixed(1)}`);
+    }
+  }
+
+  greeting("Hello World");
+  greeting(3.14);
+})()
